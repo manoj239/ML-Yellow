f9b4e187b45cca821b76386bd39b5dd684999627
manoj239 <manojkumar.borra@infosys.com>
2026-03-31 16:34:23 +0000

    python

--- train/demo.py
+++ train/demo.py
@@ -10,3 +10,8 @@ test_size=config["training"]["test_size"]
 print(test_size)
 random_state=config["training"]["random_state"]
 print(random_state)
+
+docker_image_Path=config['docker']['docker_images_path']
+docker_image_name=config['docker']['docker_image_name']
+print(docker_image_Path)
+print(docker_image_name)
